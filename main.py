df18a62a0d95e8a31462b42f8e9337142f16d16a
Undri <undribelies@gmail.com>
2019-12-06 22:02:51 +0300

    deleted stopwords

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 # sentiment analysis
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
 
 
 def main():
-
+    # чтение из файла
     source_file = open('data.txt', 'r', encoding='utf8')
-    src = source_file.readlines()
-    text = 'Hello, Mr. Andrey. How are you doing?'
-    print(nltk.sent_tokenize(text))
-    for word in nltk.word_tokenize(text):
-        print(word)
-    lines = list()
-    for line in src:
-        if line != "\n":
-            lines.append(line)
-    print(len(lines))
+    output_file = open('out.txt', 'w', encoding='utf8')
+    src = source_file.read()
+
+    # дробление текста на слова с помощью токенайзера по словам из nltk
+    words = nltk.word_tokenize(src)
+
+    # удаление нейтраньлых слов используя stopwords.words("Russian") set
+    # и сет значков
+    redundant = (',', '.', '#', '...', '!', '?', ':', '…')
+    stop_words = set(stopwords.words("Russian")).union(redundant)
+
+    filtered_words = [word for word in words if word not in stop_words]
+    # print(len(words) - len(filtered_words))
+    words = filtered_words
+    for word in words:
+        output_file.writelines(word + "\n")
     source_file.close()
+    output_file.close()
 
 
 if __name__ == '__main__':
